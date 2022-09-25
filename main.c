83d75ec7dbae0b152200704ede3afd3fcd27227a
Collin Murphy <dang@localhost.localdomain>
2022-09-24 22:41:07 -0700

    detect file paths

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int checkFilePath(char *fname){
+    FILE *file;
+    if((file = fopen(fname, "r"))){
+        fclose(file);
+        return 1;
+    }  
+    else
+        return 0;
+}
+
+void successfulArgs(char *input){
+    if(checkFilePath(input) == 1)
+        printf("Correct file path.\n");
+    else
+        printf("Invalid File path.\n");
+}
+
+int main(int argc, char *argv[]){
+    if(argc == 2){
+        printf("%s\n", argv[1]);
+        successfulArgs(argv[1]);
+    }
+    else if(argc > 2)
+        printf("Too many args\n");
+    else
+        printf("One argument expected.\n");
+    return 0;
+}
